f29e12603387c3c2783453b60d168085140d07f2
nbutluo <nbutluo@163.com>
2025-08-01 20:37:52 +0800

    初始化 vue 项目

---
+++ myVite/src/App.vue
@@ -0,0 +1,44 @@
+<!-- <script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script> -->
+
+<script>
+export default {
+  data () {
+    return {
+      name: "后盾人",
+      title: 'houdunren',
+    }
+  }
+}
+</script>
+
+<template>
+  <h1>{{ title }}--{{ name }}</h1>
+  <div>
+    <a href="https://vite.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
