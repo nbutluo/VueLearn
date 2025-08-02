b45171079b194a36558cd89b37620bfa35842c33
nbutluo <nbutluo@163.com>
2025-08-02 13:46:02 +0800

    自定义组件

---
+++ myVue2/src/components/classComponent.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="class-component">
+    <div :class="[classLists,'package']">{{ name }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: "这是后端组件",
+      active: false,
+    }
+  },
+  computed: {
+    classLists() {
+      return {
+        current: true,
+        hd: false,
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.class-component {
+  color: white;
+  background-color:  rebeccapurple;
+
+  .hd {
+    font-size: 2em;
+  }
+
+  .current {
+    color:white;
+  }
+
+  .package {
+    font-weight: bold;
+    font-size: 4em;
+  }
+}
+</style>
