89889f017dbac78aeb39d31765e16d937b4fe75f
nbutluo <nbutluo@63.com>
2025-08-03 22:48:36 +0800

    mounted声明周期钩子函数

---
+++ myVue2/src/components/ArrComponent.vue
@@ -0,0 +1,28 @@
+<template>
+  <div v-for="val in arr">
+    {{ val}}
+  </div>
+</template>
+
+<script>
+
+const arr = ['houdunren.com']
+export default {
+    data() {
+      return {
+        arr
+      }
+    },
+  mounted() {
+      setTimeout(()=>{
+          // this.arr.push('向军')
+        this.arr.slice(0,1)
+      },3000)
+
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
