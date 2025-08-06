cb26ecb718712e5e6682dc04027377e48df6ed1e
nbutluo <nbutluo@63.com>
2025-08-06 11:54:41 +0800

    props初步使用

---
+++ Component/props/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <hd-button :content="hdContent" type="success"  style="margin-right: 10px"/>
+  <hd-button content="删除" type="danger" style="margin-right: 10px"/>
+  <hd-button content="反馈" style="margin-right: 10px"/>
+</template>
+
+<script>
+import hdButton from "./components/HdButton.vue";
+
+export default {
+  components: {hdButton},
+  data() {
+    return {
+      hdContent: '保存提交'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
