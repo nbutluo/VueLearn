6c285780617b24d66733fd864ba0c42e94fcf8c5
nbutluo <nbutluo@163.com>
2025-08-11 17:20:18 +0800

    slot插槽使用

---
+++ slot/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+<!--   <card>-->
+<!--     <input type="text" :value='sdsd' id='input-hd' />-->
+<!--   </card>-->
+
+    <card>
+      <hd-button/>
+    </card>
+  </div>
+</template>
+
+<script>
+import Card from "./components/Card.vue";
+import HdButton from "./components/HdButton.vue";
+export default {
+  components:{HdButton, Card}
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ slot/src/components/HdButton.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <button>向军大叔的按钮</button>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
