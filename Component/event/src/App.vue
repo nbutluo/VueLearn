47da5fd3c9a0cae43f9164e021478364be1b878b
nbutluo <nbutluo@163.com>
2025-08-11 10:30:21 +0800

    emits触发自定义事件

---
+++ Component/event/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="lesson">
+    <lesson v-for="item in db" :lesson="item" @del="show(item)"/>
+  </div>
+</template>
+
+<script>
+import db from '../data/db.js'
+import Lesson from "./components/Lesson.vue";
+
+export default {
+  components: {Lesson},
+  data() {
+    return {
+      db,
+    }
+  },
+  methods: {
+    del(item) {
+      console.log(item)
+
+      const index = this.db.findIndex(l => l.id == item.id);
+      this.db.splice(index, 1)
+    },
+    show(item) {
+      const index = this.db.findIndex(l => l.id == item.id);
+      this.db.splice(index, 1)
+      console.log(item)
+    }
+  }
+
+}
+</script>
+
+<style lang="scss" scoped>
+.lesson {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  column-gap: 10px;
+}
+</style>
---
+++ Component/event/src/components/Lesson.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+    <img :src="lesson.preview" alt="lesson.title"/>
+    <!--    <span @click="del" v-bind="$attrs">x</span>-->
+<!--    <span @click="$emit('del',lesson)">x</span>-->
+    <span @click="del">x</span>
+
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['lesson'],
+  emits: ['del'],
+  methods: {
+    del() {
+      this.$emit('del',this.lesson);
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+div {
+  border: solid 1px #ddd;
+  text-align: center;
+  transition: 1s;
+  position: relative;
+
+  &:hover {
+    box-shadow: 0 0 20px #aaa;
+
+    > span {
+      opacity: 1;
+    }
+  }
+
+  span {
+    display: block;
+    background-color: #666;
+    color: #fff;
+    border-radius: 50%;
+    width: 20px;
+    height: 20px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: absolute;
+    top: 10px;
+    right: 10px;
+    cursor: pointer;
+    font-size: 12px;
+    opacity: 0;
+    transition: 1s;
+  }
+
+  h3 {
+    padding: 0 0 20px 0;
+    margin: 0;
+    font-size: 16px;
+  }
+
+  img {
+    width: 100%;
+  }
+}
+</style>
