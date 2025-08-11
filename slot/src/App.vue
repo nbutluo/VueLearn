eb4b007e473d78c51be998fe4c087358d5b7e3d7
nbutluo <nbutluo@163.com>
2025-08-11 18:01:26 +0800

    具名插槽

--- slot/src/App.vue
+++ slot/src/App.vue
@@ -8,10 +8,17 @@
     <!--      <hd-button/>-->
     <!--    </card>-->
 
-    <card :content="content">
-<!--      <div @click="show">插槽</div>-->
-<!--      <hd-button>保存</hd-button>-->
-      <hd-button>提交</hd-button>
+<!--    <card :content="content">-->
+<!--&lt;!&ndash;      <div @click="show">插槽</div>&ndash;&gt;-->
+<!--&lt;!&ndash;      <hd-button>保存</hd-button>&ndash;&gt;-->
+<!--      <hd-button>提交</hd-button>-->
+<!--    </card>-->
+
+<!--  具名插槽  -->
+    <card>
+      <template v-slot:header>网站小贴</template>
+      <div>后盾人是一个主张友好、分享、自由的技术交流社区。 请记住我们的口号</div>
+      <template #footer>后盾人 人人做后盾</template>
     </card>
   </div>
 </template>
--- slot/src/components/Card.vue
+++ slot/src/components/Card.vue
@@ -1,10 +1,14 @@
 <template>
   <div>
-    向军大叔
-    <div style="color: #2ecc71" @click="show(content)">
+    <header>
+      <slot name="header"/>
+    </header>
+    <main>
       <slot/>
-
-    </div>
+    </main>
+    <footer>
+      <slot name="footer"/>
+    </footer>
   </div>
 </template>
 
@@ -22,5 +26,19 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+div {
+  border: solid 1px #ddd;
 
+  header,
+  main,
+  footer {
+    padding: 10px;
+  }
+
+  main {
+    border-width: 1px 0 1px 0;
+    border-style: solid;
+    border-color: #ddd;
+  }
+}
 </style>
