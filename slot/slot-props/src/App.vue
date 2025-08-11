e0e74e20c3b019cd7c65bd8a5b05576e1067defd
nbutluo <nbutluo@163.com>
2025-08-11 18:40:31 +0800

    插槽 props

---
+++ slot/slot-props/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <lesson v-for="lesson in lessons" :key="lesson.id" :lesson="lesson"/>
+</template>
+
+<script>
+import lessons from './db.js'
+import Lesson from "./components/Lesson.vue";
+
+export default {
+  components: {Lesson},
+  data() {
+    return {
+      lessons,
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ slot/slot-props/src/components/Lesson.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    {{ lesson.title }}
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['lesson']
+}
+</script>
+
+<style lang="scss" scoped>
+div {
+  border: 1px solid #ddd;
+  margin-bottom: 10px;
+  padding: 10px;
+}
+</style>
