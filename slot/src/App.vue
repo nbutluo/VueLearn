a8b2a79d329dd48264b2ba2194f7a99023aec235
nbutluo <nbutluo@163.com>
2025-08-11 17:34:25 +0800

    插槽作用域

--- slot/src/App.vue
+++ slot/src/App.vue
@@ -1,11 +1,15 @@
 <template>
   <div>
-<!--   <card>-->
-<!--     <input type="text" :value='sdsd' id='input-hd' />-->
-<!--   </card>-->
+    <!--   <card>-->
+    <!--     <input type="text" :value='sdsd' id='input-hd' />-->
+    <!--   </card>-->
 
-    <card>
-      <hd-button/>
+    <!--    <card>-->
+    <!--      <hd-button/>-->
+    <!--    </card>-->
+
+    <card :content="content">
+      <div @click="show">插槽</div>
     </card>
   </div>
 </template>
@@ -13,8 +17,19 @@
 <script>
 import Card from "./components/Card.vue";
 import HdButton from "./components/HdButton.vue";
+
 export default {
-  components:{HdButton, Card}
+  components: {HdButton, Card},
+  data() {
+    return {
+      content: '父组件',
+    }
+  },
+  methods: {
+    show() {
+      console.log(this.content)
+    },
+  },
 }
 </script>
 
--- slot/src/components/Card.vue
+++ slot/src/components/Card.vue
@@ -1,12 +1,23 @@
 <template>
   <div>
-    <slot/>
+    向军大叔
+    <div style="color: #2ecc71" @click="show(content)">
+      <slot/>
+
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-
+  props: [
+    'content',
+  ],
+  methods: {
+    show(content) {
+      console.log(`子组件:${content}`)
+    },
+  },
 }
 </script>
 
