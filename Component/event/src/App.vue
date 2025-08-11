2b9958220db9161bede7090eca5baeee4ee852e8
nbutluo <nbutluo@163.com>
2025-08-11 12:13:29 +0800

    v-model的手写实现

--- Component/event/src/App.vue
+++ Component/event/src/App.vue
@@ -1,21 +1,43 @@
 <template>
   <div class="lesson">
     <lesson v-for="item in db" :lesson="item" @del="show"/>
+    <!--    <input type="text" :value="title" @input="title = $event.target.value">-->
+    <!--    <input type="text" :value="title" @input="input">-->
+    <!--    {{title}}-->
+<!--    <hd-input :value="title" @update:value="change"/>-->
+
+<!--    <hd-input :value="title" @update:value="title = $event"/>-->
   </div>
+  <!--  <hr>-->
+  <!--  <hd-input :value="title" />-->
+  <HdInput v-model:value="title"/>
+  <!--  <br>-->
+
+
+  父组件: {{ title }}
 </template>
 
 <script>
 import db from '../data/db.js'
 import Lesson from "./components/Lesson.vue";
+import HdInput from "./components/HdInput.vue";
+
 
 export default {
-  components: {Lesson},
+  components: {HdInput, Lesson},
   data() {
     return {
       db,
+      title: '后盾人',
     }
   },
   methods: {
+    change(v) {
+      this.title = v;
+    },
+    // input(event){
+    //   console.log(event.target.value);
+    // },
     del(item) {
       console.log(item)
 
@@ -26,8 +48,8 @@ export default {
       const index = this.db.findIndex(l => l.id == id);
       console.log(index)
       this.db.splice(index, 1)
-    }
-  }
+    },
+  },
 
 }
 </script>
---
+++ Component/event/src/components/HdInput.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <input type="text" :value="content" @input="change">
+    <div style="color: #2ecc71;display: inline-block">{{ content }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['value'],
+  emits: ['update:value'],
+  data() {
+    return {
+      content: this.value,
+    }
+  },
+  methods: {
+    change(event) {
+      this.content = event.target.value;
+      this.$emit('update:value', this.content);
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
