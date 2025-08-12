dfda164d14051007405733a0b90a5d47f42cd305
nbutluo <nbutluo@163.com>
2025-08-12 10:22:52 +0800

    动态组件切换

---
+++ provide/src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <main>
+    <div v-for="component in components" :key="component.name"
+         @click="currentComponent = component.name">
+      {{ component.title }}
+    </div>
+  </main>
+  {{ currentComponent }}
+
+  <component :is="currentComponent"/>
+
+</template>
+
+<script>
+import Weixin from "./components/Weixin.vue";
+import Pay from "./components/Pay.vue";
+
+export default {
+  components: {
+    Weixin, Pay,
+
+  },
+  data() {
+    return {
+      currentComponent: 'weixin',
+      components: [
+        {
+          name: 'weixin',
+          title: '微信管理',
+        },
+        {
+          name: 'pay',
+          title: '在线支付',
+        },
+
+      ],
+    }
+  },
+
+}
+</script>
+
+<style lang="scss" scoped>
+main {
+  display: flex;
+
+  div {
+    border: 1px solid #dddddd;
+    padding: 10px;
+    margin: 5px 10px;
+    cursor: pointer;
+  }
+}
+</style>
---
+++ provide/src/components/Weixin.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    微信
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+div {
+  border: 1px solid #bbbb;
+  padding: 10px;
+  margin-bottom: 10px;
+}
+</style>
