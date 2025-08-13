22b21279a90407ed3d49009928cde99e81fab98a
nbutluo <nbutluo@163.com>
2025-08-13 17:40:42 +0800

    setup初步使用

---
+++ composition/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <count />
+  </div>
+</template>
+
+<script>
+import count from "./components/Count.vue";
+import Count from "./components/Count.vue";
+export default {
+  components: {Count},
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ composition/src/components/Count.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <button>-</button>
+    {{ num }}
+    <button @click="add">+</button>
+  </div>
+</template>
+
+<script>
+
+import {ref} from "vue";
+
+export default {
+  setup() {
+    let num = ref(2);
+
+    let add = () => {
+      console.log(num)
+      num.value = num.value + 1;
+    }
+
+    return {num,add}
+  },
+  // methods: {
+  //   add() {
+  //     console.log(this.num)
+  //     this.num++;
+  //   },
+  // },
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
